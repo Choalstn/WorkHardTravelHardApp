c787265d1a82c9cd066791f776db2be6b9a72d4f
Choalstn <choalstn@gmail.com>
2022-08-18 03:48:59 +0900

    TouchableOpacity

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import { theme } from './color';
 
 export default function App() {
+  const [working, setWorking] = useState(true);
+  const travel = () => setWorking(false);
+  const work = () => setWorking(true);
+
+  const color = {
+    work : ""
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <View style={styles.header}>
+        <TouchableOpacity onPress={work}>
+        <Text style={{...styles.btnText, color: working ? theme.white : theme.grey}}>Work</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={travel}>
+        <Text style={{...styles.btnText, color : working ? theme.grey : theme.white}}>Travel</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -13,8 +31,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: theme.bg,
+    paddingHorizontal : 20,
   },
+
+  header : {
+    justifyContent : 'space-between', //양쪽 정렬
+    flexDirection : 'row',
+    marginTop : 100,
+  },
+
+  btnText : {
+    fontSize : 38,
+    fontWeight : "600",
+    color : 'white'
+  }
 });
